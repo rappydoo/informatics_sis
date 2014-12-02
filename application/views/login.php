791c72ea74892788049d7449a80e7ad4b767cd3c
daryl james balangigue <daryl.balangigue@redlemonph.com>
2014-12-02 16:50:26 +0800

    test

---
+++ application/views/login.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Welcome</title>
+</head>
+<body>
+
+<form method="post">
+Username: <input type="text">
+Password: <input type="password">
+
+<input type="submit">
+</form>
+
+
+
+</body>
+</html>
